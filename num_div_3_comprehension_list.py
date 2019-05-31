dc65a9badf6c64dc9629e93c2226808a15cf8edc
Pauline Robbe <42397454+probbe@users.noreply.github.com>
2019-05-31 16:09:58 +0900

    Update num_div_3_comprehension_list.py

--- num_div_3_comprehension_list.py
+++ num_div_3_comprehension_list.py
@@ -1,9 +1,4 @@
 
-# coding: utf-8
-
-# In[15]:
-
-
 #Use a List Comprehension to create a list of all numbers between 1 and 50 that are divisible by 3.
 
 [num for num in range(0,51) if num % 3 ==0]
